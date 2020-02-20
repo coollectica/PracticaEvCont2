f831059b71668b8e9db878b2ad3d5d6df3b20968
XaviGV <57351273+XaviGV@users.noreply.github.com>
2020-02-20 15:02:21 +0100

    Fix for 3.3 that closes #14

--- actividad2.js
+++ actividad2.js
@@ -253,11 +253,9 @@ try {
   } catch (e) {}
 
 /* Cas 3.3: Fragment de codi on aparegui l'operació lógica OR,
-                  Línies de la 26 a 30.
-                  La funció s'associa a l'esdeveniment a les línies
-                  27,28,29 i 30 amb || 
-
-https://developer.mozilla.org/enUS/docs/Web/JavaScript/Reference/Operators/Logical_Operators */
+Línies de la 26 a 30.
+L'operador OR s'expressa a les línees 27, 28, 29 i 30 amb ||
+https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Operadores/Operadores_l%C3%B3gicos */
 
 var raf = win.requestAnimationFrame
 || win.webkitRequestAnimationFrame
